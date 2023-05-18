15db07bdf0a8e6ffadd1a4b1c923e8e13928e552
Irina Kalinina <ira.k.163@yandex.ru>
2023-05-18 20:42:29 +0400

    Commit_new_tests

--- tests.py
+++ tests.py
@@ -22,3 +22,80 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_get_book_rating_add_new_book_twice(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Колобок')
+        collector.add_new_book('Колобок')
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_get_book_rating_add_new_book_rating_one(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Колобок')
+
+        assert collector.get_book_rating('Колобок') == 1
+
+    def test_add_new_book_rating_not_exist(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Колобок')
+
+        assert collector.books_rating['Колобок'] != None
+
+    def test_set_book_rating_change_rating_seven(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+        collector.set_book_rating('Простоквашино', 7)
+
+        assert collector.get_book_rating('Простоквашино') == 7
+
+    def test_get_books_with_specific_rating_rating_seven_two_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+        collector.set_book_rating('Простоквашино', 7)
+        collector.add_new_book('Колобок')
+        collector.set_book_rating('Колобок', 7)
+        collector.add_new_book('Снегурочка')
+        collector.set_book_rating('Снегурочка', 4)
+
+        assert len(collector.get_books_with_specific_rating(7)) == 2
+
+    def test_add_book_in_favorites_add_one_book_in_favorites_one_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+        collector.add_book_in_favorites('Простоквашино')
+
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_add_book_in_favorites_add_book_book_in_favorites(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+        collector.add_book_in_favorites('Простоквашино')
+
+        assert 'Простоквашино' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_delete_book_book_not_in_favorites(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+        collector.add_book_in_favorites('Простоквашино')
+        collector.delete_book_from_favorites('Простоквашино')
+
+        assert 'Простоквашино' not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books_add_all_books_in_favorites_in_favorites_and_in_rating_same_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Простоквашино')
+        collector.add_book_in_favorites('Простоквашино')
+        collector.add_new_book('Морозко')
+        collector.add_book_in_favorites('Морозко')
+
+        assert collector.get_list_of_favorites_books() == list(collector.get_books_rating().keys())
